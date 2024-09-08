a768d4feb768ed45ddd164b006cff3bd0cfae523
Júlio Soares <julio.soares@lais.huol.ufrn.br>
2024-09-08 20:40:40 -0300

    Feat: Criendo modelos e Serializers

--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from escola.models import Estudante, Curso
 
-# Register your models here.
+class EstudanteAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
+    list_display_links = ('id', 'nome',)
+    search_fields = ('nome',)
+    list_per_page = 20
+
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'codigo', 'descricao')
+    list_display_links = ('id', 'codigo',)
+    search_fields = ('codigo',)
+    list_per_page = 20
+
+
+admin.site.register(Estudante, EstudanteAdmin)
+admin.site.register(Curso, CursoAdmin)
